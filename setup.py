69a34f81cf287c23572b93cd01152dd3501802b9
Robert Lehmann <lehmrob@gmail.com>
2019-11-24 11:48:34 +0100

    Initial commit basic project

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+	name='photorename',
+	version='1.0.0',
+	description='bulk rename photos in a dictionary',
+	author='Robert Lehmann',
+	author_email='lehmrob@posteo.net',
+	url='https://github.com/lehmrob',
+	packages=['photorename'],
+	scripts=['photorename.py'],
+)
