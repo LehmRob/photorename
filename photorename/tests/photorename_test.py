9f89f22fffa94cd554fde76d3042cc871e0f4074
Robert Lehmann <lehmrob@gmail.com>
2019-12-24 11:19:06 +0100

    Adding unittest support

---
+++ photorename/tests/photorename_test.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+import photorename
+
+class TestRenamer(TestCase):
+    def test_is_sample(self):
+        self.assertTrue(1==1)
--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-	name='photorename',
-	version='1.0.0',
-	description='bulk rename photos in a dictionary',
-	author='Robert Lehmann',
-	author_email='lehmrob@posteo.net',
-	url='https://github.com/lehmrob',
-	packages=['photorename'],
-	scripts=['photorename.py'],
+    name='photorename',
+    version='1.0.0',
+    description='bulk rename photos in a dictionary',
+    author='Robert Lehmann',
+    author_email='lehmrob@posteo.net',
+    url='https://github.com/lehmrob',
+    packages=['photorename'],
+    scripts=['photorename.py'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
 )
