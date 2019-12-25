1ac079c39bcfcc477d09f97cd569dadeda3b8c85
Robert Lehmann <lehmrob@gmail.com>
2019-12-25 11:42:59 +0100

    Adding testfiles

--- photorename/tests/photorename_test.py
+++ photorename/tests/photorename_test.py
@@ -1,7 +1,18 @@
 from unittest import TestCase
+from pathlib import PurePath, Path
+from tempfile import mkdtemp
+from shutil import rmtree
 
 import photorename
 
 class TestRenamer(TestCase):
-    def test_is_sample(self):
+    def test_do(self):
+        output = mkdtemp()
+        testdir = PurePath.joinpath(Path.cwd(), "test_photos")
+        print(f"Testdir {testdir} Output {output}")
+        r = photorename.Renamer(testdir, "test", output)
+        r.do()
+
         self.assertTrue(1==1)
+
+        rmtree(output)
