0176940d93107a4e12a60c15bcda8bcd17eab407
Robert Lehmann <lehmrob@gmail.com>
2019-12-26 11:14:34 +0100

    Adding argument for version

--- photorename/__init__.py
+++ photorename/__init__.py
@@ -1,7 +1,9 @@
 import argparse
+import sys
 from pathlib import Path
 from .photorename import Renamer
 
+version = "1.0.0"
 
 def run():
     parser = argparse.ArgumentParser(
@@ -13,8 +15,14 @@ def run():
     parser.add_argument('-o', '--output', dest='output', default='.',
         help="""output directory for the sorted pictures, if not used default is the current 
         directory""")
+    parser.add_argument('-v', '--version', dest='version', action='store_true',
+            help="Show version")
 
     args = parser.parse_args()
+
+    if args.version:
+        print(f"{version}")
+        sys.exit(0)
 
     renamer = Renamer(args.directory, args.name, args.output)
     print(renamer)
