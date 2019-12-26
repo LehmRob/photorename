47d8d23d760ee46097d1ff72eceb4808189be215
Robert Lehmann <lehmrob@gmail.com>
2019-12-26 11:06:45 +0100

    Complete lib for photorename

--- photorename/photorename.py
+++ photorename/photorename.py
@@ -1,6 +1,23 @@
-from pathlib import Path
+from pathlib import Path, PurePath
 from typing import List
 from exif import Image
+from shutil import copy2
+
+class Photo():
+    def __init__(self, path: Path, suffix: str) -> None:
+        with open(path, 'rb') as f:
+                self.image = Image(f)
+        self.path = path
+        self.suffix = suffix
+
+    def __repr__(self) -> str:
+        return f"{self.path}"
+
+    def timestamp(self) -> str:
+        return self.image.datetime
+
+    def path(self) -> Path:
+        return self.path
 
 class Renamer:
     '''Renamer class does the actual analyzing and renaming of the picture files'''
@@ -15,39 +32,40 @@ class Renamer:
     def do(self) -> None:
         '''Do is the main entry point for doing the reading and sorting'''
         photos = self.scan_for_photos()
+        sorted_photos = sorted(photos, key=lambda photo: photo.timestamp())
+        self.copy_photos(sorted_photos)
 
-    def scan_for_photos(self) -> List[Path]:
+    def scan_for_photos(self) -> List[Photo]:
         ret = list()
         for f in self.directory.iterdir():
             if f.is_file():
                 ext = f.suffix
                 if ext == ".jpg" or ext == ".jpeg" or ext == ".JPG" or ext == ".JPEG"  or ext == ".png":
-                    photo = Photo(f)
+                    photo = Photo(f, ext)
                     ret.append(photo)
         return ret
 
+    def copy_photos(self, sorted_photos: List[Photo]) -> None:
+        counter = 1
+        for photo in sorted_photos:
+            file_name = PurePath(f"{self.base_name}_{counter:04}.{photo.suffix}")
+            target_path = PurePath.joinpath(self.output_dir, file_name)
+            copy2(photo.path, target_path)
+            counter += 1
+
+
 def prepare_output_dir(output_dir: str) -> Path:
+    p = Path(output_dir)
     if output_dir == '.':
         # using current output directory
         return Path.cwd()
     elif output_dir.find('~'):
-        p = Path(output_dir)
-    else:
-        p = Path(output_dir)
+        p = Path(output_dir).expanduser()
 
     if not p.exists():
         p.expanduser().mkdir()
     elif not p.is_dir():
         raise RuntimeError(f"{output_dir} is no directory")
 
-class Photo():
-    def __init__(self, path: Path) -> None:
-        with open(path, 'rb') as f:
-                self.image = Image(f)
-        self.path = path
+    return p
 
-    def __repr__(self) -> str:
-        return f"{self.image}"
-
-    def timestamp(self) -> str:
-        return self.image.datetime
--- photorename/tests/photorename_test.py
+++ photorename/tests/photorename_test.py
@@ -2,17 +2,42 @@ from unittest import TestCase
 from pathlib import PurePath, Path
 from tempfile import mkdtemp
 from shutil import rmtree
+from hashlib import sha256
 
 import photorename
 
+def hash_for_file(filepath: Path) -> str:
+    with open(filepath, 'rb') as f:
+        cnt = f.read()
+        hash = sha256(cnt)
+        return hash.hexdigest()
+
 class TestRenamer(TestCase):
     def test_do(self):
+        expected_order = [
+            'ee7e84526b423ad8f22269d3b1936d2f85cd7faa6f6944b86c68ffe31e31bac7',
+            '55a5d7a1cebc0ead7333b68bd12c35fbf230ce400ecfa9ba58d63e590dfd6df0',
+            '9ad51101a85ade792971e0f23f36092e4d1d929f561ed02416b1251abf2fbf64',
+        ]
+
         output = mkdtemp()
         testdir = PurePath.joinpath(Path.cwd(), "test_photos")
         print(f"Testdir {testdir} Output {output}")
         r = photorename.Renamer(testdir, "test", output)
         r.do()
+        output_dir = Path(output)
 
-        self.assertTrue(1==1)
+        files = list()
+        for f in output_dir.iterdir():
+            files.append(f)
+
+        files.sort()
+        self.assertTrue(len(files) == 3)
+        print(f"Hash for file {hash_for_file(files[0])}")
+        self.assertTrue(hash_for_file(files[0]) == expected_order[0]) 
+        print(f"Hash for file {hash_for_file(files[1])}")
+        self.assertTrue(hash_for_file(files[1]) == expected_order[1]) 
+        print(f"Hash for file {hash_for_file(files[2])}")
+        self.assertTrue(hash_for_file(files[2]) == expected_order[2]) 
 
         rmtree(output)
