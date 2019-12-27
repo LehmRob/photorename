dcf7b2957d732c358b0853c63b400f7111f47f27
Robert Lehmann <lehmrob@gmail.com>
2019-12-27 12:06:43 +0100

    Adding version file

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+from distutils.util import convert_path
+
+main_ns = {}
+vpath = convert_path('photorename/version.py')
+with open(vpath) as vfile:
+    exec(vfile.read(), main_ns)
 
 setup(
     name='photorename',
-    version='1.0.0',
+    version=main_ns['__version__'],
     description='bulk rename photos in a dictionary',
     author='Robert Lehmann',
     author_email='lehmrob@posteo.net',
